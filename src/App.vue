6f13971b6490d993d372c2a6308a70618bcb67a3
Patryk Majcher <22518267@stu.mmu.ac.uk>
2024-01-23 18:25:33 +0000

    web

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div></div>
+  </template>
+  
+  <script>
+  // the relevant methods
+  import { doc, setDoc } from "firebase/firestore"
+  import db from './firebase/init.js'
+  
+  export default {
+    created() {
+      this.createCountry()
+    },
+    methods: {
+      async createCountry() {
+  
+        await setDoc(doc(db, 'countries', 'US'), {
+          name: 'United States'
+        })
+      }
+    }
+  }
+  </script>
